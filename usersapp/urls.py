65a14486281e85b281dd4d3883fca63baade1172
“Andrzej <asidor23@gmail.com>
2023-06-27 23:38:58 +0200

    Set Navbar logic and Login/Logut

--- usersapp/urls.py
+++ usersapp/urls.py
@@ -1,10 +1,10 @@
-
-
-
 from django.urls import path
 from .views import RegisterView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("login/",auth_views.LoginView.as_view(template_name='userapp/login.html'), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(template_name='userapp/logout.html'),name='logout'),
     path("register/", RegisterView.as_view(), name='register'),
 ]
 
